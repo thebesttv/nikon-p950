5434853ef6a73153af35ea094bb2c702096d1be9
thebesttv <taojuntian@outlook.com>
2024-06-01 14:27:02 +0800

    Use argparser

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
-import sys
 import subprocess
 import json
 from datetime import datetime
 import pytz
+import argparse
 
 CMD = '''
 ffmpeg -y -hide_banner \\
@@ -21,8 +21,6 @@ ffmpeg -y -hide_banner \\
     {output}
 '''
 
-ROTATE_LEFT = False
-
 def get_ffprobe_info(filename):
     command = ['ffprobe', '-v', 'quiet', '-print_format', 'json', '-show_format', '-show_streams', filename]
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -32,8 +30,11 @@ def get_ffprobe_info(filename):
 def get_tag(d, tag):
     return d['tags'][tag]
 
-def handle_video(filename):
-    filename = "DSCN2040.MP4"
+def handle_video(filename, output, rotate_left):
+    print("=========================================")
+    print(f"= Input:  {filename}")
+    print(f"= Output: {output}")
+    print("=========================================")
     info = get_ffprobe_info(filename)
 
     creation_time = get_tag(info['format'], 'creation_time')
@@ -55,22 +56,25 @@ def handle_video(filename):
     cmd = CMD.format(
         input=filename,
         ctime=formatted_time,
-        output='output.mp4',
-        extra_vf=',transpose=2' if ROTATE_LEFT else '',
+        output=output,
+        extra_vf=',transpose=2' if rotate_left else '',
     )
     os.system(cmd)
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print('Usage: main.py [-left] VIDEO...')
-        sys.exit(1)
+    parser = argparse.ArgumentParser(
+        description='Nikon P950 video compression tool')
+    parser.add_argument('-l', '--left', action='store_true',
+                        help='Rotate all videos 90 degrees to the left')
+    parser.add_argument('videos', nargs='+', help='Videos to compress')
 
-    for filename in sys.argv[1:]:
-        if filename == '-left':
-            ROTATE_LEFT = True
-            break
-    
-    for filename in sys.argv[1:]:
-        if filename == '-left':
-            continue
-        handle_video(filename)
+    args = parser.parse_args()
+
+    for filename in args.videos:
+        # split into directory, basename, extension
+        directory, basename = os.path.split(filename)
+        basename, extension = os.path.splitext(basename)
+        print(directory, basename, extension)
+        output = os.path.join(directory, f'{basename}_c{extension}')
+
+        handle_video(filename, output, args.left)
